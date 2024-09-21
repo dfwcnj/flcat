687eaff8ca9d73cb4e1dfb7178f08635552d26b2
don caldwell <doncaldwell@dons-Laptop.local>
2024-09-21 14:39:43 -0400

    .

--- flcat.go
+++ flcat.go
@@ -8,7 +8,13 @@ import (
 	"os"
 )
 
-func flcat(fn string, rlen int) {
+// process a fixed length string record file
+// func flcat(fn string, rlen, koff, klen int) {
+// fn name of file to process
+// rlen record length
+// koff offset of key in rexord
+// klen record key length
+func flcat(fn string, rlen, koff, klen int) {
 	var err error
 
 	var fsz, rsz int64
@@ -25,11 +31,17 @@ func flcat(fn string, rlen int) {
 		}
 		fsz = finf.Size()
 		defer ifp.Close()
+	} else {
+		// ifp = os.Stdin
+		ifp, err = os.Open("/dev/stdin")
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	r := make([]byte, rlen)
 	for {
-		if rsz == fsz {
+		if fsz != 0 && rsz == fsz {
 			return
 		}
 		_, err := ifp.Read(r)
@@ -39,22 +51,33 @@ func flcat(fn string, rlen int) {
 			}
 			log.Fatal(err)
 		}
-		fmt.Println(string(r))
+		if koff == 0 && klen == 0 {
+			fmt.Println(string(r))
+		} else {
+			fmt.Println(string(r[koff : koff+klen]))
+		}
 		rsz += int64(rlen)
 	}
 }
 
 func main() {
 	var fn string
-	var rlen int
-	flag.StringVar(&fn, "fn", "", "-fm name of fl file to emit")
-	flag.IntVar(&rlen, "rlen", 0, "-f name of fl file to emit")
+	var rlen, koff, klen int
+	flag.StringVar(&fn, "fn", "", "name of fl file to emit")
+	flag.IntVar(&rlen, "rlen", 0, "record length")
+	flag.IntVar(&koff, "koff", 0, "offset of key in record")
+	flag.IntVar(&rlen, "rlen", 0, "record key length")
 	flag.Parse()
 
 	if rlen == 0 {
 		log.Fatal("rlen must be supplied")
 	}
+	if koff != 0 || klen != 0 {
+		if koff+klen > rlen {
+			log.Fatal("key must fall within record bounds")
+		}
+	}
 
-	flcat(fn, rlen)
+	flcat(fn, rlen, koff, klen)
 
 }
